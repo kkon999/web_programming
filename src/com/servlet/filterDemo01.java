724f0683589deabae186cac39c0f2502f59f58db
kkon999 <gon910910@gmail.com>
2020-04-12 22:31:32 +0900

    JSP-Filter 주석

--- src/com/servlet/filterDemo01.java
+++ src/com/servlet/filterDemo01.java
@@ -14,13 +14,13 @@ import javax.servlet.annotation.WebServlet;
 @WebServlet("/filter")
 public class filterDemo01 implements Filter  {
 	
-	@Override
+	@Override // filter가 초기화 될 때 실행되는 메소드
 	public void init(FilterConfig arg0) throws ServletException {
 		System.out.println(" --filter init()--");
 		
 	}
 	
-	@Override
+	@Override // filter가 실제 작동되는 메소드
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 	    System.out.println("-- filter doFilter()--");
@@ -34,7 +34,7 @@ public class filterDemo01 implements Filter  {
 	}
 		
 	
-	@Override
+	@Override // filter가 종료 될 때 실행되는 메소드
 	public void destroy() {
 		System.out.println(" --filter destroy()--");
 	}  
